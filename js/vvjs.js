0df363730f060f361e6b124064e641a6859766ff
Alaa Haddad <drupaldeveloper@live.com>
2024-08-08 22:25:54 -0500

    Update arrows

--- js/vvjs.js
+++ js/vvjs.js
@@ -21,6 +21,16 @@
       let autoSlideIntervalId = null;
       let isPaused = false;
 
+      const playIconSVG = `
+        <svg class="svg-play" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="inherit">
+          <path d="m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+        </svg>`;
+
+      const pauseIconSVG = `
+        <svg class="svg-pause" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="inherit">
+          <path d="M360-320h80v-320h-80v320Zm160 0h80v-320h-80v320ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+        </svg>`;
+
       const manageAutoSlideInterval = (action, parentId, slideTime) => {
         if (action === 'clear') {
           if (autoSlideIntervalId) {
@@ -188,18 +198,23 @@
 
           const togglePlayPause = () => {
             isPaused = !isPaused;
+
+            // Clear any existing content
+            playPause.innerHTML = '';
+
             if (isPaused) {
               playPause.classList.replace('play', 'pause');
-              playPause.innerHTML = '&#9654;';
+              playPause.innerHTML = playIconSVG; // Set to play icon
               playPause.setAttribute('aria-label', 'Start automatic slide show');
               manageAutoSlideInterval('clear');
             } else {
               playPause.classList.replace('pause', 'play');
-              playPause.innerHTML = '&#10073;&nbsp;&#10073;';
+              playPause.innerHTML = pauseIconSVG; // Set to pause icon
               playPause.setAttribute('aria-label', 'Stop automatic slide show');
               manageAutoSlideInterval('start', parentId, slideTime);
             }
           };
+
 
           stopOnHover.addEventListener('mouseover', () => {
             if (!isPaused) {
